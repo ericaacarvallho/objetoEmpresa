8fb50841ad2ab09adfe4d1cd2333c4d27c036f13
ericaacarvalho <ericaemiliano@hotmail.com>
2024-08-29 20:17:57 -0300

    Objeto Empresa

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 const companies = [
     {
-      name: 'Amazon',
-      founded: 1994,
-      industry: ['E-Commerce', 'Cloud'],
+    name: 'Amazon',
+    founded: 1994,
+    industry: ['E-Commerce', 'Cloud'],
     },
     {
-      name: 'Facebook',
-      founded: 2004,
-      industry: ['Social'],
+    name: 'Facebook',
+    founded: 2004,
+    industry: ['Social'],
     },
     {
-      name: 'Alphabet Inc.',
-      founded: 2015,
-      industry: ['Search', 'Cloud', 'Advertising'],
+    name: 'Alphabet Inc.',
+    founded: 2015,
+    industry: ['Search', 'Cloud', 'Advertising'],
     },
-  ];
-  
-  companies.sort((a, b) => a.name.localeCompare(b.name));
-  
-  companies.forEach((company) => {
+];
+
+companies.sort((a, b) => a.name.localeCompare(b.name));
+
+companies.forEach((company) => {
     company.kind = 'Internet company';
-  });
-  
-  const MAX_NAME_LENGHT = Math.max(
+});
+
+const MAX_NAME_LENGHT = Math.max(
     ...companies.map((company) => company.name.length)
-  );
-  
-  function show(companies) {
+);
+
+function show(companies) {
     return companies
-      .map(
+    .map(
         (company) =>
-          company.name.padEnd(MAX_NAME_LENGHT + 3, '.') + company.founded
-      )
-      .join('\n');
-  }
-  
-  // console.table(companies);
-  console.log(show(companies));
+        company.name.padEnd(MAX_NAME_LENGHT + 3, '.') + company.founded
+    )
+    .join('\n');
+}
+
+console.log(show(companies));
